bb79930b021144f9ef4755e6c4283247281fb770
Rich Harris <richard.a.harris@gmail.com>
2013-11-03 13:35:17 +0000

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,76 @@
+module.exports = function ( grunt ) {
+
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON( 'package.json' ),
+
+		jshint: {
+			main: 'src/**/*.js',
+			options: {
+				strict: true,
+				unused: true,
+				undef: true,
+				smarttabs: true,
+				globals: {
+					define: true,
+					module: true,
+					require: true,
+					window: true,
+					document: true
+				}
+			}
+		},
+
+		concat: {
+			bundle: {
+				src: 'src/Ractive-decorators-sortable.js',
+				dest: 'tmp/Ractive-decorators-sortable.js'
+			},
+			options: {
+				process: {
+					data: {
+						VERSION: '<%= pkg.version %>'
+					}
+				}
+			}
+		},
+
+		qunit: {
+			files: [ 'test/index.html' ]
+		},
+
+		uglify: {
+			bundle: {
+				src: 'tmp/Ractive-decorators-sortable.js',
+				dest: 'tmp/Ractive-decorators-sortable.min.js'
+			}
+		},
+
+		copy: {
+			bundle: {
+				files: [{
+					cwd: 'tmp/',
+					src: '*.js',
+					dest: '',
+					expand: true
+				}]
+			}
+		}
+		
+	});
+
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-contrib-concat' );
+	grunt.loadNpmTasks( 'grunt-contrib-qunit' );
+	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+	grunt.loadNpmTasks( 'grunt-contrib-copy' );
+
+	grunt.registerTask( 'default', [
+		'jshint',
+		'concat',
+		'qunit',
+		'uglify',
+		'copy'
+	]);
+
+};
---
+++ test/tests.js
@@ -0,0 +1,13 @@
+// Ractive-decorators-sortable tests
+// ===========================
+
+(function () {
+
+	var fixture;
+
+	test( 'We are ready to write some tests', function ( t ) {
+		t.ok( true );
+	});
+
+
+}());
